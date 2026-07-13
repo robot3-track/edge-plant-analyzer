8655c91387591171993cc1cb17c2cfffbf122324
robot3-track <yohanyinyuchang@gmail.com>
2026-07-13 09:34:07 -0700

    add android compilation files

---
+++ android/app/src/main/java/com/yourname/plantanalyzer/MainActivity.java
@@ -0,0 +1,5 @@
+package com.yourname.plantanalyzer;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourname.plantanalyzer',
+  appName: 'edge-plant-analyzer',
+  webDir: 'out'
+};
+
+export default config;
